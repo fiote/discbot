e05732aff348ea6c8ccb342a47d21dc843d5d1e7
fiote <murilo.eng@gmail.com>
2023-04-16 22:23:42 -0300

    xxx - adjust notifyCardMove to always send the message regardless of the symbol

--- src/services/disco.ts
+++ src/services/disco.ts
@@ -376,10 +376,9 @@ export default class Disco {
 			symbol = '🟨';
 		}
 
-		if (symbol) {
-			await this.renameThread(thread, Disco.prependSymbol(symbol, thread.name));
-			await this.send(thread.id, `**${author}** moveu o card #${card_id} do trello para a lista **[${listAfter}]**.`);
-		}
+		if (symbol) await this.renameThread(thread, Disco.prependSymbol(symbol, thread.name));
+
+		await this.send(thread.id, `**${author}** moveu o card #${card_id} do trello para a lista **[${listAfter}]**.`);
 	}
 
 	// ===== LOG ====================================================
